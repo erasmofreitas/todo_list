e491a6b858dc0a21e12ef63f3ab8e834bce6469a
Erasmo Freitas <erasmo.sf@gmail.com>
2022-09-19 19:48:06 -0300

    utilizando controladores de texto

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -5,8 +5,28 @@ class TodoListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-    );
+    return Scaffold(
+        body: Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: TextField(
+          decoration: InputDecoration(
+            labelText: 'Preço',
+            //hintText: 'exemplo@exemplo.com',
+            //border: OutlineInputBorder(),
+            errorText: null,
+            prefixText: 'R\$ ',
+            suffixText: 'cm',
+          ),
+          keyboardType: TextInputType.number,
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+            color: Colors.purple,
+          ),
+          //obscureText: false,
+        ),
+      ),
+    ));
   }
 }
