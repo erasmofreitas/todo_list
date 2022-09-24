2ba75170cc590a48bf131bbfca3567640d0d9e74
Erasmo Freitas <erasmo.sf@gmail.com>
2022-09-23 21:56:04 -0300

    ajustando campo texto

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,48 +1,34 @@
 import 'package:flutter/material.dart';
 
 class TodoListPage extends StatelessWidget {
-  TodoListPage({super.key});
-
-  final TextEditingController emailController = TextEditingController();
+  const TodoListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Padding(
+      body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            TextField(
-              controller: emailController,
-              decoration: InputDecoration(
-                labelText: 'E-mail',
+        child: Center(
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Adicione uma tarefa',
+                    hintText: 'Ex. Estudar FLutter',
+                  ),
+                ),
               ),
-              onChanged: onChanged,
-              onSubmitted: onSubmitted,
-            ),
-            ElevatedButton(
-              onPressed: login,
-              child: Text('Entrar'),
-            )
-          ],
+              SizedBox(width: 8),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('+'),
+              ),
+            ],
+          ),
         ),
       ),
-    ));
-  }
-
-  void login() {
-    String text = emailController.text;
-    print(text);
-    emailController.text = 'clicou';
-  }
-
-  void onChanged(String text) {
-    //print(text);
-  }
-
-  void onSubmitted(String text) {
-    print(text);
+    );
   }
 }
