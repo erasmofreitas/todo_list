6d5e45dfcf12cab4a8da30e6903bcc4a46d0ef15
Erasmo Freitas <erasmo.sf@gmail.com>
2022-09-25 12:15:16 -0300

    implementando flutter_slidable

--- lib/widgets/todo_list_item.dart
+++ lib/widgets/todo_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:intl/intl.dart';
 import 'package:todo_list/models/todo.dart';
 
@@ -9,30 +10,43 @@ class TodoListItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(4),
-        color: Colors.grey[200],
-      ),
-      margin: const EdgeInsets.symmetric(vertical: 2),
-      padding: const EdgeInsets.all(10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+    return Slidable(
+      endActionPane: const ActionPane(
+        extentRatio: 0.25,
+        motion: DrawerMotion(),
         children: [
-          Text(
-            DateFormat('dd/MM/yyyy - HH:mm:ss').format(todo.dateTime),
-            style: TextStyle(
-              fontSize: 12,
-            ),
-          ),
-          Text(
-            todo.title,
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
+          SlidableAction(
+            onPressed: null,
+            backgroundColor: Colors.red,
+            icon: Icons.delete,
           ),
         ],
+      ),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(4),
+          color: Colors.grey[200],
+        ),
+        margin: const EdgeInsets.symmetric(vertical: 2),
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              DateFormat('dd/MM/yyyy - HH:mm:ss').format(todo.dateTime),
+              style: TextStyle(
+                fontSize: 12,
+              ),
+            ),
+            Text(
+              todo.title,
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
